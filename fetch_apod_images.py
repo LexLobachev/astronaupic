bbed38b463401fbaa274a00eb24a0b04e9e9432e
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2022-12-02 19:54:19 +0400

    add condition on media type

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -18,7 +18,9 @@ def fetch_multiple_nasa_pics(dir_name, url, api_key, count):
 
     for pic_number in range(count):
         pic_url = response.json()[pic_number]['url']
-        download_pic(dir_name, picture_name, pic_url, pic_number)
+        media_type = response.json()[pic_number]['media_type']
+        if media_type == 'image':
+            download_pic(dir_name, picture_name, pic_url, pic_number)
 
 
 if __name__ == '__main__':
